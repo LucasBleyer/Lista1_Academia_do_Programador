71e5859c20dbdc0792754201b9860b068e8acf16
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-02-07 23:01:26 -0300

    Add project files.

---
+++ Imobiliaria.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Imobiliaria.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
