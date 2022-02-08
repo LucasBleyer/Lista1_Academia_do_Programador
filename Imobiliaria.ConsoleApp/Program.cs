82c9016f56e8315c65f1d5e998f76aa349b38d31
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-02-07 23:02:41 -0300

    add imoboliaria

--- Imobiliaria.ConsoleApp/Program.cs
+++ Imobiliaria.ConsoleApp/Program.cs
@@ -6,7 +6,16 @@ namespace Imobiliaria.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Digite a largura do terreno: ");
+            double largura = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite a altura do terreno: ");
+            double altura = Convert.ToDouble(Console.ReadLine());
+
+            double resultado = largura * altura;
+
+            Console.WriteLine(resultado);
+            Console.ReadLine();
         }
     }
 }
