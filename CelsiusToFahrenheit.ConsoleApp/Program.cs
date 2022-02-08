e532b49dcafd4f391d8312f3f16af8fed73eb7db
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-02-07 23:17:10 -0300

    add c to f

---
+++ CelsiusToFahrenheit.ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CelsiusToFahrenheit.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Temperatura em Celsius: ");
+            double c = Convert.ToDouble(Console.ReadLine());
+
+            double f = (c * 1.8) + 32;
+
+            Console.Write("Temperatura em Fahrenheit: " + f);
+            Console.ReadLine();
+        }
+    }
+}
