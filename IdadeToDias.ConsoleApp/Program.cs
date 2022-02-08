4926c38ec6e00d41f76eb43aaa8ed66e8bf31864
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-02-07 23:05:45 -0300

    add idade to dias

---
+++ IdadeToDias.ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace IdadeToDias.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite seu nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Digite sua idade: ");
+            int idade = Convert.ToInt32(Console.ReadLine());
+
+            int dias_vividos = idade * 365;
+
+            Console.Write(nome + " você já viveu " + dias_vividos + " dias!");
+
+            Console.ReadLine();
+        }
+    }
+}
